64ede54de3bb359f4bfb406f9aa41dd3922d6290
Phaeyz <phaeyz@pm.me>
2025-03-08 11:59:42 -0800

    Update test dependencies (#2)

--- DocGenerator/generate-docs.cmd
+++ DocGenerator/generate-docs.cmd
@@ -1,5 +1,5 @@
 setlocal
-set lib=Phaeyz.Collections
+set libName=Phaeyz.Collections
 set repoUrl=https://github.com/Phaeyz/Collections
-dotnet run ..\%lib%\bin\Debug\net9.0\%lib%.dll ..\docs --source %repoUrl%/blob/main/%lib% --namespace %lib% --clean
+dotnet run ..\%libName%\bin\Debug\net9.0\%libName%.dll ..\docs --source %repoUrl%/blob/main/%libName% --namespace %libName% --clean
 endlocal
